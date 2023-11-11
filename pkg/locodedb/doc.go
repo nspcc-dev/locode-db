fc4bb3753ee3699d75009a591801cdabeb512e51
Roman Khimov <roman@nspcc.ru>
2023-11-11 19:29:32 +0300

    locodedb: add minimalistic package doc
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

---
+++ pkg/locodedb/doc.go
@@ -0,0 +1,7 @@
+/*
+Package locodedb implements a UN LOCODE database.
+
+It contains all the data internally and provides simple [Get] API to retrieve
+records based on short LOCODE strings.
+*/
+package locodedb
