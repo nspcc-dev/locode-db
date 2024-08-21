bfe1c41407edef485b37afaaa42fedd8bad6d716
Andrey Butusov <andrey@nspcc.io>
2024-08-21 18:52:14 +0300

    locode-db: drop cobra dependency
    Use default flag package instead of cobra.
    
    Closes #34.
    
    Signed-off-by: Andrey Butusov <andrey@nspcc.io>

--- internal/generate/generate.go
+++ internal/generate/generate.go
@@ -1,16 +1,30 @@
 package generate
 
 import (
+	"flag"
+	"fmt"
+	"log"
+
 	locode "github.com/nspcc-dev/locode-db/internal/parsers/db"
 	airportsdb "github.com/nspcc-dev/locode-db/internal/parsers/db/airports"
 	continentsdb "github.com/nspcc-dev/locode-db/internal/parsers/db/continents/geojson"
 	csvlocode "github.com/nspcc-dev/locode-db/internal/parsers/table/csv"
-	"github.com/spf13/cobra"
 )
 
 type namesDB struct {
 	*airportsdb.DB
 	*csvlocode.Table
+}
+
+type stringSliceFlag []string
+
+func (s *stringSliceFlag) String() string {
+	return fmt.Sprint(*s)
+}
+
+func (s *stringSliceFlag) Set(value string) error {
+	*s = append(*s, value)
+	return nil
 }
 
 const (
@@ -23,68 +37,79 @@ const (
 )
 
 var (
-	locodeGenerateInPaths        []string
+	locodeGenerateInPaths        stringSliceFlag
 	locodeGenerateSubDivPath     string
 	locodeGenerateAirportsPath   string
 	locodeGenerateCountriesPath  string
 	locodeGenerateContinentsPath string
 	locodeGenerateOutPath        string
-
-	locodeGenerateCmd = &cobra.Command{
-		Use:   "generate",
-		Short: "Generate UN/LOCODE database",
-		Args:  cobra.NoArgs,
-		Run: func(cmd *cobra.Command, _ []string) {
-			locodeDB := csvlocode.New(
-				csvlocode.Prm{
-					Path:       locodeGenerateInPaths[0],
-					SubDivPath: locodeGenerateSubDivPath,
-				},
-				csvlocode.WithExtraPaths(locodeGenerateInPaths[1:]...),
-			)
-
-			airportDB := airportsdb.New(airportsdb.Prm{
-				AirportsPath:  locodeGenerateAirportsPath,
-				CountriesPath: locodeGenerateCountriesPath,
-			})
-
-			continentsDB := continentsdb.New(continentsdb.Prm{
-				Path: locodeGenerateContinentsPath,
-			})
-
-			targetDB := locode.New(locodeGenerateOutPath)
-
-			names := &namesDB{
-				DB:    airportDB,
-				Table: locodeDB,
-			}
-
-			err := locode.FillDatabase(locodeDB, airportDB, continentsDB, names, targetDB)
-			if err != nil {
-				cmd.PrintErrln(err)
-			}
-		},
-	}
 )
 
-func initUtilLocodeGenerateCmd() {
-	flags := locodeGenerateCmd.Flags()
+func init() {
+	flag.Var(&locodeGenerateInPaths, locodeGenerateInputFlag, "List of paths to UN/LOCODE tables (CSV)")
+	flag.StringVar(&locodeGenerateSubDivPath, locodeGenerateSubDivFlag, "", "Path to UN/LOCODE subdivision database (CSV)")
+	flag.StringVar(&locodeGenerateAirportsPath, locodeGenerateAirportsFlag, "", "Path to OpenFlights airport database (CSV)")
+	flag.StringVar(&locodeGenerateCountriesPath, locodeGenerateCountriesFlag, "", "Path to OpenFlights country database (CSV)")
+	flag.StringVar(&locodeGenerateContinentsPath, locodeGenerateContinentsFlag, "", "Path to continent polygons (GeoJSON)")
+	flag.StringVar(&locodeGenerateOutPath, locodeGenerateOutputFlag, "", "Target path for generated database (directory))")
+}
 
-	flags.StringSliceVar(&locodeGenerateInPaths, locodeGenerateInputFlag, nil, "List of paths to UN/LOCODE tables (CSV)")
-	_ = locodeGenerateCmd.MarkFlagRequired(locodeGenerateInputFlag)
+func Execute() {
+	flag.Parse()
 
-	flags.StringVar(&locodeGenerateSubDivPath, locodeGenerateSubDivFlag, "", "Path to UN/LOCODE subdivision database (CSV)")
-	_ = locodeGenerateCmd.MarkFlagRequired(locodeGenerateSubDivFlag)
+	if err := validateFlags(); err != nil {
+		log.Fatal(err)
+	}
 
-	flags.StringVar(&locodeGenerateAirportsPath, locodeGenerateAirportsFlag, "", "Path to OpenFlights airport database (CSV)")
-	_ = locodeGenerateCmd.MarkFlagRequired(locodeGenerateAirportsFlag)
+	locodeDB := csvlocode.New(
+		csvlocode.Prm{
+			Path:       locodeGenerateInPaths[0],
+			SubDivPath: locodeGenerateSubDivPath,
+		},
+		csvlocode.WithExtraPaths(locodeGenerateInPaths[1:]...),
+	)
 
-	flags.StringVar(&locodeGenerateCountriesPath, locodeGenerateCountriesFlag, "", "Path to OpenFlights country database (CSV)")
-	_ = locodeGenerateCmd.MarkFlagRequired(locodeGenerateCountriesFlag)
+	airportDB := airportsdb.New(airportsdb.Prm{
+		AirportsPath:  locodeGenerateAirportsPath,
+		CountriesPath: locodeGenerateCountriesPath,
+	})
 
-	flags.StringVar(&locodeGenerateContinentsPath, locodeGenerateContinentsFlag, "", "Path to continent polygons (GeoJSON)")
-	_ = locodeGenerateCmd.MarkFlagRequired(locodeGenerateContinentsFlag)
+	continentsDB := continentsdb.New(continentsdb.Prm{
+		Path: locodeGenerateContinentsPath,
+	})
 
-	flags.StringVar(&locodeGenerateOutPath, locodeGenerateOutputFlag, "", "Target path for generated database (directory))")
-	_ = locodeGenerateCmd.MarkFlagRequired(locodeGenerateOutputFlag)
+	targetDB := locode.New(locodeGenerateOutPath)
+
+	names := &namesDB{
+		DB:    airportDB,
+		Table: locodeDB,
+	}
+
+	err := locode.FillDatabase(locodeDB, airportDB, continentsDB, names, targetDB)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func validateFlags() error {
+	for _, inPath := range locodeGenerateInPaths {
+		if inPath == "" {
+			return fmt.Errorf("no UN/LOCODE table specified")
+		}
+	}
+	switch {
+	case len(locodeGenerateInPaths) == 0:
+		return fmt.Errorf("at least one UN/LOCODE table is required")
+	case locodeGenerateSubDivPath == "":
+		return fmt.Errorf("path to UN/LOCODE subdivision database is required")
+	case locodeGenerateAirportsPath == "":
+		return fmt.Errorf("path to OpenFlights airport database is required")
+	case locodeGenerateCountriesPath == "":
+		return fmt.Errorf("path to OpenFlights country database is required")
+	case locodeGenerateContinentsPath == "":
+		return fmt.Errorf("path to continent polygons is required")
+	case locodeGenerateOutPath == "":
+		return fmt.Errorf("target path for generated database is required")
+	}
+	return nil
 }
